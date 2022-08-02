6b585bafa5dbc65afd11becdc1b06c071afb1603
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-08-02 14:58:03 +0100

    auth routes

---
+++ routes/authRoutes.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const router = express.Router()
+const { login, logout, register } = require('../controllers/authController')
+
+router.route('/login').post(login)
+router.route('/register').post(register)
+router.route('/logout').get(logout)
+
+module.exports = router
