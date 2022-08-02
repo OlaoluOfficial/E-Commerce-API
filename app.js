02b20b472b32dd963bc659cf7582d4665c43d417
OlaoluOfficial <olaoluwasamuel96@yahoo.com>
2022-08-02 14:50:01 +0100

    app

---
+++ app.js
@@ -0,0 +1,65 @@
+const express = require('express')
+const notFoundMiddleware = require('./middleware/not-found')
+const errorHandlerMiddleware = require('./middleware/error-handler')
+require('express-async-errors')
+require('dotenv/config')
+const logger = require('morgan')
+const connectDB = require('./db/connect')
+const { StatusCodes } = require('http-status-codes')
+const authRouter = require('./routes/authRoutes')
+const cookieParser = require('cookie-parser')
+const userRouter = require('./routes/userRoutes')
+const productRouter = require('./routes/productRoutes')
+const reviewRouter = require('./routes/reviewRoutes')
+const orderRouter = require('./routes/orderRoutes')
+const fileUpload = require('express-fileupload')
+const rateLimiter = require('express-rate-limit')
+const helmet = require('helmet')
+const xss = require('xss-clean')
+const cors = require('cors')
+const mongoSanitize = require('express-mongo-sanitize')
+
+const app = express()
+app.use(express.json())
+app.use(logger('dev'))
+app.set('trust proxy', 1)
+app.use(rateLimiter({
+  windowMs: 15 * 60 * 1000,
+  max: 60
+}))
+app.use(helmet())
+app.use(xss())
+app.use(cors())
+app.use(mongoSanitize())
+app.use(cookieParser(process.env.JWT_SECRET))
+app.use(express.static('./public'))
+app.use(fileUpload())
+
+const port = process.env.PORT || 3000
+
+app.get('/api/v1', (req,res) => {
+  // console.log(req.cookies)
+  console.log(req.signedCookies)
+  res.status(StatusCodes.OK).send('Home page')
+})
+
+app.use('/api/v1/auth', authRouter)
+app.use('/api/v1/users', userRouter)
+app.use('/api/v1/products', productRouter)
+app.use('/api/v1/reviews', reviewRouter)
+app.use('/api/v1/orders', orderRouter)
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI, console.log('Connected to db'))
+    app.listen(port, () => {
+      console.log(`Server is listening on port ${port}`)
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+start()
